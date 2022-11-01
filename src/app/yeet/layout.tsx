1b53fd53030322513eb512624af6b71d4d8d1028
Louis Giët <louis@digitalastronaut.be>
2022-11-01 18:59:30 +0100

    Commit before changing folder structure to use next13 layouts

--- src/app/yeet/layout.tsx
+++ src/app/yeet/layout.tsx
@@ -1,16 +1,20 @@
 import { useSession } from "next-auth/react"
 import { PropsWithChildren } from "react"
+import { getServerAuthSession } from "../../server/common/get-server-auth-session"
+import {} from "next-auth"
 
 interface Props {
     
 }
 
 
-const Layout: React.FC<PropsWithChildren<Props>> = ({children}) => {
-    const {data} = useSession({
-        required: true
-    })
+const Layout: React.FC<PropsWithChcildren<Props>> = async ({children}) => {
+    // const {data} = useSession({
+    //     required: true
+    // })
+    const session = await getServerAuthSession({
 
+    })
     return (
         <div>
             {JSON.stringify(data)}
